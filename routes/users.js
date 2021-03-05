ee3fff33fce4dce55a3c3c859fb64ee0399e8990
oussama maaroufi <oussama.maaroufi@esprit.tn>
2021-03-05 22:34:10 +0100

    first commit

---
+++ routes/users.js
@@ -0,0 +1,147 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const User = require('../models/user.js')
+const passport = require('passport');
+
+
+//Login
+router.post('/auth', (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const query = { email}
+  //Check the user exists
+  User.findOne(query, (err, user) => {
+    //Error during exuting the query
+    if (err) {
+      return res.send({
+        success: false,
+        message: 'Error, please try again'
+      });
+    }
+
+    //No User match the search condition
+    if (!user) {
+      return res.send({
+        success: false,
+        message: 'Error, Account not found'
+      });
+    }
+
+    //Check if the password is correct
+    user.isPasswordMatch(password, user.password, (err, isMatch) => {
+
+        //Invalid password
+        if (!isMatch) {
+          return res.send({
+            success: false,
+            message: 'Error, Invalid Password'
+          });
+        }
+
+        //User is Valid
+
+        const ONE_WEEK =  3000 ;//604800; //Token validtity in seconds
+
+        //Generating the token
+        const token = jwt.sign({ user }, process.env.SECRET, { expiresIn: ONE_WEEK });
+
+        //User Is Valid
+        //This object is just used to remove the password from the retuned fields
+        let returnUser = {
+          name: user.name,
+          email: user.email,
+          type :user.type,
+          id: user._id
+        }
+
+        //Send the response back
+        return res.send({
+          success: true,
+          message: 'You can login now',
+          user: returnUser,
+          token
+        });
+    });
+
+  });
+
+});
+
+//Registeration
+router.post('/register', (req, res, next) => {
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    type:req.body.type,
+    password: req.body.password
+  });
+  
+
+  newUser.save((err, user) => {
+    if (err) {
+      console.log(err);
+      return res.send({
+        success: false,
+        message: 'Failed to save the user'
+      });
+    }
+    if (!user) {
+      return res.send({
+        success: false,
+        message: 'Error, Invalid user'
+      });
+    }
+    res.send({
+      success: true,
+      message: 'User Saved',
+      user
+    });
+  });
+});
+
+
+//find , passport.authenticate('jwt', { session : false})
+
+router.get('/list' ,(req, res, next) => {
+  //const owner = req.body.owner;
+  console.log('test')
+  User.find((err, users)=>{
+    if (err) {
+      return res.send({
+        success: false,
+        message: 'Error while reteriving the user'
+      });
+    }
+
+    return res.send({
+     // success: true,
+      users
+    });
+  });
+});
+
+// /getuser
+router.post('/getuser', (req, res, next) => {
+  let _id  = req.body.id;
+  let query = {_id}
+  console.log(query);
+  User.findById( query ,(err, user)=>{
+    if (err) {
+      return res.send({
+        success: false,
+        message: 'Error while reteriving the user'
+      });
+    }
+    console.log(user);
+    return res.send({
+      success: true,
+      user
+    // user
+    });
+  });
+});
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,62 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodayParser = require('body-parser');
+const passport = require('passport');
+//Intiailzie app with express
+const app = express();
+
+const UserRoutes = require('./routes/users');
+
+
+//Database Connection
+mongoose.Promise = global.Promise; // Fix Deprecation issue
+mongoose.connect(process.env.DATABASE,{ useNewUrlParser: true , useUnifiedTopology: true });
+
+//Port to be used by the server
+const _PORT = process.env.PORT;
+const mongoURI = process.env.DATABASE;
+
+// Create mongo connection
+const conn = mongoose.createConnection(mongoURI,{ useNewUrlParser: true , useUnifiedTopology: true });
+let gfs;
+
+
+conn.on('connected', () => {
+
+  console.log('Connected to the database');
+});
+conn.on('error',  (err) => {
+  console.log(`Unable to connect to the database: ${err}`);
+});
+
+
+//---------------- Middlewares ----------------//
+//CROS MW
+app.use(cors());
+
+//Body Parser MW
+
+app.use(bodayParser.json());
+
+//Passport MW
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/passport')(passport);
+//---------------- Middlewares ----------------//
+
+
+//Index Rotuer
+app.get('/', (req, res, next) => {
+  res.send('I am alive')
+});
+
+//Users Routes
+app.use('/users', UserRoutes);
+//Start the server jfdnjfjg
+app.listen(_PORT, () => {
+  console.log('Server Started');
+});
+
